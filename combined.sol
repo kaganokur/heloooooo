2fcc4a1fc5c0268ea15a919855ee5fa542000719
kaganokur <159198900+kaganokur@users.noreply.github.com>
2024-02-08 03:57:32 +0300

    Update combined.sol

--- combined.sol
+++ combined.sol
@@ -446,4 +446,14 @@ abstract contract Context {
         return 0;
     }
 }
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KukiToken is ERC20 {
+    constructor() ERC20("Kuki", "KU") {
+        _mint(msg.sender, 100000000 * 10 ** uint(decimals()));
+    }
+}
 
